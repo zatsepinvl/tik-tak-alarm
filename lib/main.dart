2bd8cca244a0e76b9b39094465545f77ed51d42b
Maksim <xmakss@mail.ru>
2019-03-21 01:16:18 +0300

    added math alarm (disable by multiplying)

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:math';
 
 /*
  * Alarm example: https://github.com/Yahhi/pomodoro_on_flutter
@@ -17,29 +19,29 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome to Flutter'),
+          title: Text('Welcome to CSF Alarm'),
         ),
         body: Center(
           child: Alarm(),
         ),
       ),
-
     );
   }
 }
-
 
 class AlarmState extends State<Alarm> {
   String alarmingText = "Alarm!";
   int duration = 0;
   Timer counter;
   AudioPlayer currentPlayer;
+  BuildContext context;
 
   static AudioCache player = new AudioCache();
   static const alarmAudioPath = "alarm.mp3";
 
   @override
   Widget build(BuildContext context) {
+    this.context = context;
     if (counter == null) {
       counter = new Timer.periodic(Duration(seconds: 1), handleTimeout);
     }
@@ -56,17 +58,15 @@ class AlarmState extends State<Alarm> {
                     onPressed: () {
                       DatePicker.showTimePicker(context, showTitleActions: true,
                           onConfirm: (date) {
-                            setState(() {
-                              duration = date.second;
-                            });
-                          },
-                          currentTime: DateTime.now()
-                      );
+                        setState(() {
+                          duration = date.second;
+                        });
+                      }, currentTime: DateTime.now());
                     },
-                    child: Text('Setup alarm')
-                ),
+                    child: Text('Setup alarm')),
                 FlatButton(
                   onPressed: () {
+
                     stopAlarm();
                   },
                   child: Text("Stop alarm"),
@@ -81,20 +81,81 @@ class AlarmState extends State<Alarm> {
 
   void handleTimeout(Timer timer) {
     if (duration == 1) {
+      showStopDialog();
       startAlarm();
       setState(() {
         duration = 0;
       });
-    }
-    else if (duration > 1) {
+    } else if (duration > 1) {
       setState(() {
         duration--;
       });
     }
   }
 
+  final _text = TextEditingController();
+
+  void showStopDialog() async {
+    var rnd = new Random();
+    int a = rnd.nextInt(100);
+    int b = rnd.nextInt(100);
+    String trueAns = (a + b).toString();
+
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return SimpleDialog(
+            title: const Text("Let's to stop it!", textAlign: TextAlign.center,),
+            children: <Widget>[
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("How much is $a + $b?"),
+                    new TextField(
+                      controller: _text,
+                      onChanged: (a) => checkAnswer(trueAns, context),
+                      style: new TextStyle(fontSize: 15.0, color: Colors.black),
+                      decoration: new InputDecoration(
+                          hintText: "Answer",
+                          labelStyle: TextStyle(fontWeight: FontWeight.w700),
+                          contentPadding: const EdgeInsets.all(20.0)),
+                      autofocus: true,
+                      keyboardType:
+                          TextInputType.numberWithOptions(signed: true),
+
+                    )
+                  ]),
+
+//              Row(
+//                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                  children: <Widget>[
+//                    FlatButton(
+//                      onPressed: () {
+//                        stopAlarm();
+//                        Navigator.pop(context);
+//                      },
+//                      child: const Text('Stop'),
+//                    ),
+//                  ])
+            ],
+          );
+        });
+  }
+
+  void checkAnswer(String trueAnswer, BuildContext context) {
+    if (_text.text == trueAnswer) {
+      stopAlarm();
+      _text.clear();
+      Navigator.pop(context);
+    } else {
+      if (trueAnswer.length == _text.text.length) _text.clear();
+    }
+  }
+
   void startAlarm() {
-    player.loop(alarmAudioPath, mode: PlayerMode.LOW_LATENCY)
+    player
+        .loop(alarmAudioPath, mode: PlayerMode.LOW_LATENCY)
         .then((currentPlayer) {
       this.currentPlayer = currentPlayer;
     });
@@ -107,6 +168,15 @@ class AlarmState extends State<Alarm> {
     setState(() {
       duration = 0;
     });
+    Fluttertoast.showToast(
+        msg: "Great!🏆 Wake up ASAP",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        timeInSecForIos: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0
+    );
   }
 }
 
